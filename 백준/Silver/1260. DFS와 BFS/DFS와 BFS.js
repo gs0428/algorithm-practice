70bd206fc7b438cf075b50db12e30d880fefbc0e
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-05-26 21:04:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 208 ms, Memory: 16524 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const [N, M, V] = input.shift().split(" ").map(Number);
+const arr = Array.from({ length: 1001 }, () => []);
+input.forEach((i) => {
+  const [a, b] = i.split(" ").map(Number);
+  arr[a].push(b);
+  arr[b].push(a);
+});
+
+const bfsAns = [];
+const bfsVisited = Array.from({ length: 1001 }, () => false);
+const dfsAns = [];
+const dfsVisited = [...bfsVisited];
+
+function dfs(num) {
+  if (dfsVisited[num]) return;
+  dfsVisited[num] = true;
+  dfsAns.push(num);
+  const shouldVisit = arr[num].sort((a, b) => a - b);
+  for (const visit of shouldVisit) {
+    dfs(visit);
+  }
+}
+
+dfs(V);
+
+function bfs(num) {
+  if (bfsVisited[num]) return;
+  bfsVisited[num] = true;
+  bfsAns.push(num);
+  const queue = arr[num].sort((a, b) => a - b);
+  while (queue.length > 0) {
+    const item = queue.shift();
+    if (bfsVisited[item]) continue;
+    bfsVisited[item] = true;
+    bfsAns.push(item);
+    const newArr = arr[item].sort((a, b) => a - b);
+    queue.push(...newArr);
+  }
+}
+
+bfs(V);
+
+console.log(`${dfsAns.join(" ")}\n${bfsAns.join(" ")}`);
