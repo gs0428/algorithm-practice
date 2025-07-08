a5086a4ad6c0eab892244c45c5752dd620d41b8a
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-07-08 10:51:29 +0900

    [Silver I] Title: IOIOI, Time: 188 ms, Memory: 15448 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.js
+++ 백준/Silver/5525. IOIOI/IOIOI.js
@@ -2,20 +2,21 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = +input.shift();
-const str = "I" + "OI".repeat(N);
-const len = str.length;
+const len = 2 * N + 1;
 const M = +input.shift();
+const S = input.shift();
 let start = 0;
 let cnt = 0;
-while (start <= M - len) {
-  let flag = false;
-  for (let i = 0; i < len; i++) {
-    if (input[0][i + start] !== str[i]) {
-      flag = true;
-      break;
-    }
+let ans = 0;
+
+while (start < M - 1) {
+  if (S.slice(start, start + 3) === "IOI") {
+    if (++cnt >= N) ans++;
+    start += 2;
+  } else {
+    cnt = 0;
+    start++;
   }
-  if (!flag) cnt++;
-  start++;
 }
-console.log(cnt);
+
+console.log(ans);
