a2e32ca2fe68bf555376bfee59f9946a161e0c97
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-09-01 15:24:57 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.js
@@ -1,25 +1,32 @@
 function solution(n, w, num) {
-    const arr = Array.from({length: n}, (_,i) => i + 1);
-    const floorArr = [];
-    const totalFloor = n % w ? Math.ceil(n / w) : n / w;
-    const targetFloor = num % w ? Math.floor(num / w) : num / w - 1;
-    
-    for(let i = 0;i < totalFloor; i++) {
-        if(i === totalFloor - 1 && arr.length !== w) {
-            arr.push(...Array.from({length: w - arr.length}, () => 0));
+    let totalHeight = Math.floor(n % w === 0 ? n / w : n / w + 1);
+    let cnt = 1;
+    const boxes = Array.from({length: totalHeight}, (_,idx) => {
+        const oneLineBoxes = Array.from({length: w}, (_,innerIdx) => {
+            const num = w * idx + innerIdx + 1;
+            if(num > n) return -1;
+            return num;
+        })
+        if(idx % 2) {
+            return oneLineBoxes.reverse()
         }
-        const slicedArr = arr.splice(0, w);
-        if(i % 2) floorArr.push(slicedArr.reverse())
-        else floorArr.push(slicedArr)
-        
+        return oneLineBoxes;
+    })
+    
+    const targetFloor = Math.floor(num % w === 0 ? num / w - 1 : num / w);
+    const index = boxes[targetFloor].indexOf(num);
+    
+    while(1) {
+        if(boxes[totalHeight-1][index] === -1) {
+            totalHeight--;
+            continue;
+        } else if(boxes[totalHeight-1][index] === num) {
+            break;
+        }
+        cnt++;
+        totalHeight--;
     }
     
-    const targetIndex = floorArr[targetFloor].indexOf(num);
     
-    const answer = floorArr.reduce((acc, cur) => {
-        if(cur[targetIndex]) return acc += 1;
-        return acc;
-    }, 0)
-    
-    return answer - targetFloor;
+    return cnt;
 }
