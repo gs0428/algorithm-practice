64b9d525345ec365d8e2d20085c16b35570ddc7a
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-09-11 14:46:09 +0900

    [level 2] Title: 피로도, Time: 8.35 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,43 +1,29 @@
-
-
 function solution(k, dungeons) {
-    let answer = 0;
+    let answer = 1;
     const dungeonSize = dungeons.length;
     
     function swap(a, b) {
         [dungeons[b], dungeons[a]] = [dungeons[a], dungeons[b]];
     }
     
-    function calculateFatigue() {
-        let currentFatigue = k;
-        let i = 0;
-        for(i;i<dungeonSize;i++) {
-            if(currentFatigue >= dungeons[i][0]) {
-                currentFatigue -= dungeons[i][1]
-            } else if(currentFatigue < dungeons[i][0]) {
-                break;
-            }
-        }
-        answer = Math.max(i, answer)
-    }
-    
     function permutaion(depth, fatigue) {
         if(depth === dungeonSize) {
+            answer = depth;
             return;
         }
 
-        calculateFatigue()
-        
+        answer = Math.max(depth, answer)
+
         for(let i = depth;i < dungeonSize;i++) {
             swap(i, depth);
-            permutaion(depth + 1, fatigue - dungeons[i][1]);
+            if(fatigue >= dungeons[depth][0]) {
+                permutaion(depth + 1, fatigue - dungeons[depth][1]);
+            }
             swap(i, depth);
         }
     }   
     
     permutaion(0, k);
-    
-    console.log(answer);
     
     return answer;
 }
