8f198f82e043188aeff40674e07869445e645dde
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-09-22 08:17:23 +0900

    [Silver V] Title: 셀프 넘버, Time: 208 ms, Memory: 12160 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -11,10 +11,9 @@ for (let i = 1; i < 10001; i++) {
       number
         .toString()
         .split("")
-        .map(Number)
-        .reduce((acc, cur) => acc + cur, 0) + number;
+        .reduce((acc, cur) => acc + Number(cur), 0) + number;
 
-    if (number <= 10000) isSelfNumberArr[newNumber] = false;
+    if (newNumber <= 10000) isSelfNumberArr[newNumber] = false;
 
     number = newNumber;
   }
