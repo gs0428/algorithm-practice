e4135f20e0eeb5759eae5bed99cf887acc1906df
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-10-10 21:16:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,34 @@
+const strToNum = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three:	3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+}
+
+function solution(s) {
+    if(!isNaN(+s)) return BigInt(s)
+    
+    let ans = "";
+    let str = "";
+    
+    for(const item of s) {
+        // 숫자인 경우
+        if(!isNaN(+item)) {
+            ans += item;
+        } else {
+            str += item;
+            if(strToNum[str] !== undefined) {
+                ans += strToNum[str];
+                str = "";
+            }   
+        }
+    }
+    
+    return BigInt(ans);
+}
