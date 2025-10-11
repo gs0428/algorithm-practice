174f5de6151bc2a075fda8da3e7435c6c0b6ff1f
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-10-11 21:06:16 +0900

    [Silver II] Title: 스택 수열, Time: 208 ms, Memory: 33740 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -1,32 +1,28 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath, "utf8").trim().split("\n");
+
 const n = +input.shift();
-const nums = input.map(Number);
-let curIdx = 0;
-const stack = [];
+
 const ans = [];
-const calc = [];
-for (let i = 1; i <= n; i++) {
-  stack.push(i);
-  calc.push("+");
-  if (nums[curIdx] === i) {
-    while (1) {
-      const last = stack[stack.length - 1];
-      const last2 = stack[stack.length - 2];
-      ans.push(last);
-      stack.pop();
-      calc.push("-");
-      curIdx++;
-      if (nums[curIdx] !== last2 || curIdx >= n || !last2) break;
-    }
+const stack = [];
+
+let curNum = 1;
+
+for (let i = 0; i < n; i++) {
+  const num = +input[i];
+
+  while (curNum <= num) {
+    stack.push(curNum++);
+    ans.push("+");
+  }
+
+  const pop = stack.pop();
+  ans.push("-");
+
+  if (pop !== num) {
+    return console.log("NO");
   }
 }
 
-const resArr = Array.from({ length: stack.length }, () => "-");
-const ansStr = JSON.stringify([...ans, ...stack.reverse()]);
-const inputStr = JSON.stringify(nums);
-if (ansStr === inputStr) {
-  return console.log([...calc, ...resArr].join("\n"));
-}
-console.log("NO");
+console.log(ans.join("\n"));
