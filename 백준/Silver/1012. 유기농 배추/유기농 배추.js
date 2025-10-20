65066464f71e7911bb1464fb4a6810fc9ed55133
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-10-21 08:52:44 +0900

    [Silver II] Title: 유기농 배추, Time: 184 ms, Memory: 13744 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,50 +1,65 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-const T = +input.shift();
+const N = +input[0];
+
+let i = 1;
 const ans = [];
-for (let i = 0; i < T; i++) {
-  let [M, N, K] = input.shift().split(" ").map(Number);
-  const earthworms = input.splice(0, K).map((i) => i.split(" ").map(Number));
-  while (earthworms.length > 0) {
-    const start = earthworms.shift();
-    const queue = [start];
-    while (queue.length > 0) {
-      const item = queue.shift();
-      const xRightIndex = earthworms.findIndex(
-        (e) => e[0] === item[0] + 1 && e[1] === item[1]
-      );
-      if (xRightIndex !== -1) {
-        const loc = earthworms.splice(xRightIndex, 1);
-        K--;
-        queue.push(loc[0]);
+while (i < input.length) {
+  // M은 가로 길이, N은 세로 길이, K는 위치의 개수
+  const [M, N, K] = input[i].split(" ").map(Number);
+
+  const visited = Array.from({ length: N }, () =>
+    Array.from({ length: M }, () => false)
+  );
+  const cabbages = Array.from({ length: N }, () =>
+    Array.from({ length: M }, () => 0)
+  );
+
+  let cnt = 0;
+
+  for (let j = 1; j <= K; j++) {
+    const [X, Y] = input[j + i].split(" ").map(Number);
+    cabbages[Y][X] = 1;
+  }
+
+  function bfs(location) {
+    const queue = [location];
+    visited[location[1]][location[0]] = true;
+
+    let head = 0;
+    while (head < queue.length) {
+      const [x, y] = queue[head++];
+      if (x > 0 && !visited[y][x - 1] && cabbages[y][x - 1] === 1) {
+        visited[y][x - 1] = true;
+        queue.push([x - 1, y]);
       }
-      const yRightIndex = earthworms.findIndex(
-        (e) => e[0] === item[0] && e[1] === item[1] + 1
-      );
-      if (yRightIndex !== -1) {
-        const loc = earthworms.splice(yRightIndex, 1);
-        K--;
-        queue.push(loc[0]);
+      if (y > 0 && !visited[y - 1][x] && cabbages[y - 1][x] === 1) {
+        visited[y - 1][x] = true;
+        queue.push([x, y - 1]);
       }
-      const xLeftIndex = earthworms.findIndex(
-        (e) => e[0] === item[0] - 1 && e[1] === item[1]
-      );
-      if (xLeftIndex !== -1) {
-        const loc = earthworms.splice(xLeftIndex, 1);
-        K--;
-        queue.push(loc[0]);
+      if (x < M - 1 && !visited[y][x + 1] && cabbages[y][x + 1] === 1) {
+        visited[y][x + 1] = true;
+        queue.push([x + 1, y]);
       }
-      const yLeftIndex = earthworms.findIndex(
-        (e) => e[0] === item[0] && e[1] === item[1] - 1
-      );
-      if (yLeftIndex !== -1) {
-        const loc = earthworms.splice(yLeftIndex, 1);
-        K--;
-        queue.push(loc[0]);
+      if (y < N - 1 && !visited[y + 1][x] && cabbages[y + 1][x] === 1) {
+        visited[y + 1][x] = true;
+        queue.push([x, y + 1]);
       }
     }
   }
-  ans.push(K);
+
+  for (let j = 0; j < N; j++) {
+    for (let k = 0; k < M; k++) {
+      if (!visited[j][k] && cabbages[j][k] === 1) {
+        cnt++;
+        // x, y 형식으로 전달
+        bfs([k, j]);
+      }
+    }
+  }
+  ans.push(cnt);
+  i += K + 1;
 }
+
 console.log(ans.join("\n"));
